115953f056673cfd4628ce29e96cb8aee07c000a
Ianmacdonald1989 <127326567+Ianmacdonald1989@users.noreply.github.com>
2024-02-21 19:09:04 +0000

    links ready for reggex with new excel layout

--- scripts/link_edit.py
+++ scripts/link_edit.py
@@ -7,27 +7,26 @@ import re
 # import links from excel
 wb = openpyxl.load_workbook("catalogue_database_3.xlsx")
 sheet = wb.active
-links = [(cell[0].value, cell[1].value) for cell in sheet.iter_rows(min_row=2, max_row=3, min_col=1, max_col=2)]
+links = [(cell[0].value, cell[1].value, cell[2].value) for cell in sheet.iter_rows(min_row=3, max_row=4, min_col=1, max_col=3)]
 print(links[1])
 
 
 
 # retrive web links
-for text, link in links:  # Iterate over the links from the Excel sheet
+for text, middle_value, link in links:  # Iterate over the links from the Excel sheet
     response = requests.get(link)
     response.raise_for_status()
     html_content = response.text
     webbrowser.open(link)
 
-#reggex
+#reggex 
 soup = BeautifulSoup(html_content, 'html.parser')
 for tag in soup.find_all(True):
-    tag.string = re.sub(r'\s+',', tag.string')
+    tag.string = re.sub(r'\s+', ',', tag.string)
+    
+    #remove archive column
+    if tag.name == 'h3' and all(child.name == 'li' for child in tag.find_all(True)):
+        tag.extract()
 
+cleaned_html = str(soup)
 
-#   Fetch and clean the HTML
-#   cleaned_data = fetch_and_parse(link)  # function being called 
-
-#   if cleaned_data:
-#     # logic for processing 
-#     print(f"Cleaned data from {link}: {cleaned_data}")
